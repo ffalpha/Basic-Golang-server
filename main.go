3d6d621f843b58bd5a913efd17576f2eec4dc303
ffalpha <kalanam217@gmail.com>
2022-05-15 04:07:12 +0530

    Final touch

--- main.go
+++ main.go
@@ -6,8 +6,27 @@ import (
 	"net/http"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParsForm() err %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST Request Succsful\n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name=%s\n", name)
+	fmt.Fprintf(w, "Address=%s\n", address)
+}
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello !")
 }
 
 func main() {
